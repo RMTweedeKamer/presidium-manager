6654522d367dbd5ac7b00afdd7638d2b2b736a01
th8 <tspel@live.nl>
2019-09-20 18:09:36 +0200

    Resolved unintended redirect on bad api request

--- src/main/java/nl/th8/presidium/scheduler/controller/SchedulerController.java
+++ src/main/java/nl/th8/presidium/scheduler/controller/SchedulerController.java
@@ -8,10 +8,12 @@ import nl.th8.presidium.scheduler.service.NotificationService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.mvc.support.DefaultHandlerExceptionResolver;
 
 import javax.validation.Valid;
 import java.security.Principal;
@@ -54,7 +56,7 @@ public class SchedulerController {
             logger.info("Put kamerstuk " + kamerstuk.getCallsign() + " in queue for " + kamerstuk.getPostDate());
         }
         else {
-            return ResponseEntity.status(403).build();
+            return ResponseEntity.status(401).build();
         }
         return ResponseEntity.status(201).body(id);
     }
