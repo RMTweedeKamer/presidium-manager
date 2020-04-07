0f37b178520a2c8a23002c33bd649c09dcab760b
th8 <tspel@live.nl>
2020-04-07 15:41:22 +0200

    Fixed timezone issues

--- src/main/java/nl/th8/presidium/App.java
+++ src/main/java/nl/th8/presidium/App.java
@@ -7,12 +7,14 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
 import java.util.Date;
+import java.util.TimeZone;
 
 @AutoConfigureBefore
 @SpringBootApplication
 @EnableScheduling
 public class App {
     public static void main(String[] args) {
+        TimeZone.setDefault(TimeZone.getTimeZone("Europe/Amsterdam"));
         StatDTO.setStartDate(new Date());
         SpringApplication.run(App.class, args);
     }
